a5a9fa68f6ed1c44c948ee9ef46816953a17dc44
Valeria365 <valeria@365scores.com>
2021-09-05 13:29:41 +0300

    remove console log

--- src/components/image-gallery/ImageGallery.js
+++ src/components/image-gallery/ImageGallery.js
@@ -14,7 +14,6 @@ const ImageGallery = () => {
       orientation === "vertical" ? "vertical" : "horizontal",
     ].join(" "),
   };
-  console.log(`Loading state ${GalleryStore.isLoading}`);
   return (
     <>
       <div {...containerProps}>
